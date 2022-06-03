9c5ea6be8769e69f22d32e389dddebb285b97dc3
Javier Ramirez Gomez <javi.rmzg1@gmail.com>
2022-06-02 20:54:48 -0500

    adds three files

---
+++ 01-Git/homework/Notas.js
@@ -0,0 +1,58 @@
+/*Notas.
+Editor de texto: 
+Interfaz de Usuario (UI)
+Como interectuamos con la pc
+CLI: Command Line Interface:
+Terminal consiste un prom... Espacio donde tiramos comando...
+nos devuelve un output en la pantalla y da el proximo PROM: Tirar comando.
+GUI: Graphical user interface:Representar archivo,carpetas etc etc
+NUI: Nativi user interface Hablarle a la pc ,gestos ,pantalla tactil.
+Personas normales estamos acostumbradas a la GUI Ejemplo Windows etc.. todo automatizado.
+------------------------------------------------------------------------------------------------
+command [-options(s)] [argument(s)]
+Nombre del comando.--Opciones-Argumentos.
+
+Comandos:
+head: Mostar el numero de #Lineas en el archivo de texto.
+pwd: Donde estamos parados... 
+date: Fecha y dia
+ls: Muestra las carpetas del lugar.
+ls -la:Muestra archivos ocultos y mas informacion de los archivos.
+clear: Borra el output.
+cd: moverme entre carpetas.
+mkdir _Nombre de la carpeta_: Crea directorio /o "carpeta"
+rm argumento/ : Borra la carpeta
+rm -rf argumento: Fuerza borrar la carpeta...
+-----------------------------------------------------------------------
+PROS: 
+*Requiere pocos recursos.
+*Facil de automatizar.
+*Preferido por expertos.
+Muchas mas opciones de comandos que la GUI
+
+CONTRA:No es intuitivo.
+Curva de aprendizaje lenta.
+Preferido por expertos.
+No es linda visualmente.
+---------------------------------------------------------------------------
+HERRAMIENTAS GIT
+Que es git: Sistema de control de version:
+Archivos de textos...
+---------------------------------------------------------------------------
+COMANDOS GIT:
+git init:Inicia un repositorio
+Crear un archivo
+Git version:
+Git status: Como esta nuestro repositorio? que incluye en el.
+Comando,Argumentos
+-------------------------------------------------------------------------------
+Estados de archivos:
+Untrack: No estas o son cosas nuevas.
+Unmodified: Los tenia en el repositorio y no estan modificados.
+Modified: Se hizo un cambio en un archivo que yo tenia.
+Staged: Guardar en mi proximo snapshot----Commit
+----------------------------------------------------------------------------------
+Como se hace un commit?
+git add: De todos que estan... que yo modifique quiero meterlos en la proxima foto.
+*/
+
---
+++ 01-Git/homework/prueba1.js
@@ -0,0 +1,5 @@
+var Suma = 1+1
+
+Suma = 3
+
+console.log(Suma)
